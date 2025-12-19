51996a05d85fda7a7c3dbefa4803baaa7a2c0555
THEimpactt <hammadgurchani69@gmail.com>
2025-12-19 05:25:44 -0800

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import SignDawg from './assets/signdawg'
-import LogDawg from './assets/logdawg'
-import Dashdash from './assets/Dashdash'
-
-  let routy=createBrowserRouter(
-    [
-      {
-        path:'/login',
-        element: <LogDawg/>
-      },
-      {
-        path:'/',
-        element: <SignDawg/>
-      },
-      {
-        path:'/dashboard',
-        element: <Dashdash/>
-      },
-    ]
-  )
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import SignDawg from './assets/signdawg';
+import LogDawg from './assets/logdawg';
+import Dashdash from './assets/Dashdash';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={routy}/>
-  </StrictMode>,
-)
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<SignDawg />} />
+        <Route path="/login" element={<LogDawg />} />
+        <Route path="/dashboard" element={<Dashdash />} />
+      </Routes>
+    </HashRouter>
+  </StrictMode>
+);
